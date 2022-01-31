491688a3fc7ee5e7fafd6b141bf65ec148cb3257
Kevin Ly <kevinly630@gmail.com>
2022-01-30 20:04:53 -0500

    fix typo and add validations

--- src/configuration/configuration.service.ts
+++ src/configuration/configuration.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 @Injectable()
 export class ConfigurationService {
 
+    @IsString()
+    @IsNotEmpty()
     pgHost: string;
+
+    @IsNumber()
+    @IsNotEmpty()
     pgPort: number;
+
+    @IsString()
+    @IsNotEmpty()
     pgUser: string;
+
+    @IsString()
+    @IsNotEmpty()
     pgPasword: string;
+
+    @IsString()
+    @IsNotEmpty()
     pgDatabase: string;
+
+    @IsString()
+    @IsNotEmpty()
     jwtSecret: string;
 
     constructor(){
       this.pgHost = process.env.DB_HOST ?? '';
-      this.pgPort = process.env.DB_PORT? parseInt(process.env.TRAP_PORT, 10): 5432;
+      this.pgPort = process.env.DB_PORT? parseInt(process.env.DB_PORT, 10): 5432;
       this.pgDatabase = process.env.TRAP_DB;
       this.pgUser = process.env.DB_USERNAME ?? '';
       this.pgPasword = process.env.DB_PASSWORD ?? '';
